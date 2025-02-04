ba33a38a1468f13c3b03a5260f9cfc7dc7909c76
Jake Gutteridge <141460588+jakelgutteridge@users.noreply.github.com>
2025-02-04 19:24:15 +0000

    Update header-footer.js

--- header-footer.js
+++ header-footer.js
@@ -18,18 +18,9 @@ const headerHTML = `
 const footerHTML = `
   <footer>
     <center>
-      <table cellpadding="3px 0px 3px 0px" cellspacing="0px" style="width: 100%; max-width: 950px; padding-top: 10px;">
-        <tr>
-          <td>
-            <a href="https://goo.gl/maps/qGNjaJsKsYP7dyQz8" target="_blank" style="color: #ffffff;">Cedars Park, Theobalds Lane, Cheshunt EN8 8RU</a>
-          </td>
-          <td>
-            <span style="float: right;">
-              <small>2019&ndash;2023</small>
-            </span>
-          </td>
-        </tr>
-      </table>
+      <small>
+        &copy; Jake Gutteridge 2025
+      </small>
     </center>
   </footer>
 `;
